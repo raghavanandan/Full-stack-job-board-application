22cc64165f9a435f10a221d0b9e50d36665d399e
Raghav Anandan <raghav@Raghavs-MacBook-Pro.local>
2018-05-03 23:27:44 -0700

    Bootstrap commit

--- backend/api/user-api.js
+++ backend/api/user-api.js
@@ -7,11 +7,7 @@ const {User} = require('../models/users');
 
 module.exports = (app, upload) => {
 
-  cloudinary.config({
-    cloud_name: 'jobboard',
-    api_key: '834296229844959',
-    api_secret: '3vFqT0defzwuT6c7RuchG_YkuFY'
-  });
+  
 
   /********Get all_users**********/
   app.get('/users', (req, res) => {
@@ -208,7 +204,7 @@ module.exports = (app, upload) => {
         res.send(400).send(err);
       });
     })
-    
+
   });
 
   var authenticate = (req, res, next) => {
