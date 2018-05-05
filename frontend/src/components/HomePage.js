e259a38f6b652bb86cdd161d0e3d9e877f1e2756
Ajay Chauhan <chauhanajayvikram1992@gmail.com>
2018-05-05 16:44:39 -0700

    changes

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,11 +1,54 @@
 import React, {Component} from 'react';
 
 class HomePage extends Component {
-  render() {
-    return (
-      <h1>Home content</h1>
-    )
-  }
+    render() {
+        return (
+            <div className="top">
+
+                {/*{<nav class="navbar navbar-inverse navbar-fixed-top">
+                <div class="container-fluid">
+                    <div class="navbar-header">
+                        <a class="navbar-brand" href="#">Job Board</a>
+                    </div>
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="#">Home</a></li>
+                        <li><a href="#">Dashboard</a></li>
+                        <li><a href="#">Profile</a></li>
+                        <li><a href="#">Jobs</a></li>
+                        <li><a href="#">Company</a></li>
+                        <li><a href="#">Jobs</a></li>
+
+                    </ul>
+                </div>
+            </nav>}*/}
+                <h1>Jobs Board</h1>
+                <div className="logo">
+
+
+                </div>
+                <div className="modal-footer">  <div className="container">
+                    <div className="row">
+                        <h3>About US</h3>
+                        <div className="left-div col-sm-8">
+                            <div>
+                                <image ></image>
+                                <p>Raghav Anandan: Computer Engineering SJSU</p>
+                            </div>
+                            <div>
+                                <p>Mangesh Ashtankar: Computer Engineering SJSU</p>
+                                <image></image>
+                            </div>
+                            <div>
+                                <image></image>
+                                <p>Ajay Chauhan: Software Engineering SJSU San Jose, California</p>
+                            </div>
+                        </div>
+                    </div>
+                </div></div>
+
+            </div>
+        )
+    }
 }
 
 export default HomePage;
